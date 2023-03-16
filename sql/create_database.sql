ade679a5fe647fef66dc71345e58ce0dea39a8be
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2023-03-16 11:33:22 -0300

    create table with check

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -18,6 +18,15 @@ create table account_job(
 	hire_date TIMESTAMP
 );
 
+create table employees( 
+	emp_id SERIAL primary key,
+	first_name VARCHAR(50) not null,
+	last_name VARCHAR(50) not null,
+	birthdate DATE check (birthdate > '1900-01-01'),
+	hire_date date check (hire_date > birthdate),
+	salary integer check (salary > 0)
+);
+
 
 -- ALTER TABLE
 
